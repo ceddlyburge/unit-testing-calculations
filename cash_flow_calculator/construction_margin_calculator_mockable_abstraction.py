e894d5091088b036a77d871e7ea4ba1f7c8f7fbe
ceddlyburge <ceddlyburge@gmail.com>
2021-01-28 13:13:40 +0000

    Split calculator with mockable abstraction from test

---
+++ cash_flow_calculator/construction_margin_calculator_mockable_abstraction.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from typing import List
+from dataclasses import dataclass
+from cash_flow_calculator.cash_flow_step import CashFlowStep
+
+# This class calculates a subset of properties on CashFlowSteps. In reality
+# there would be multiple similar classes, all setting properties on CashFlowStep
+class ConstructionMarginCalculatorMockableAbstraction:
+    def __init__(
+            self, 
+            balance_of_plant_costs_at_financial_close: float, 
+            development_cost: float, 
+            turbine_costs: float, 
+            special_capital_costs: float, 
+            date_of_financial_close: datetime, 
+            in_selling_mode: bool, 
+            epc_margin: float, 
+            inflation_calculator
+        ):
+        self.balance_of_plant_costs_at_financial_close = balance_of_plant_costs_at_financial_close
+        self.development_cost = development_cost
+        self.turbine_costs = turbine_costs
+        self.special_capital_costs = special_capital_costs
+        self.date_of_financial_close = date_of_financial_close
+        self.in_selling_mode = in_selling_mode
+        self.epc_margin = epc_margin
+        self.inflation_calculator = inflation_calculator
+
+    def calculate_step(self, step: CashFlowStep, fraction_of_spend: float):
+        inflation = self.inflation_calculator.inflation_to(step.start_of_step)
+
+        if step.start_of_step == self.date_of_financial_close:
+            step.special_capital_costs = self.special_capital_costs
+
+            if self.in_selling_mode == False:
+                step.development_cost_if_owning = self.development_cost
+
+            step.development_cost = self.development_cost
+
+        step.turbine_cost_including_margin = \
+            self.turbine_costs * inflation * fraction_of_spend
+        
+        step.balance_of_plant_cost_including_margin = \
+            self.balance_of_plant_costs_at_financial_close * inflation * fraction_of_spend
+
+        if self.in_selling_mode:
+            step.construction_profit = \
+                -1 * \
+                (step.turbine_cost_including_margin + step.balance_of_plant_cost_including_margin) * \
+                self.epc_margin
+            
+            step.turbine_cost_including_margin *= (1 + self.epc_margin)
+            
+            step.balance_of_plant_cost_including_margin *= (1 + self.epc_margin)
