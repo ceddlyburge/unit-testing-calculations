e6619ef554322257b4958d7145926573ce332e26
ceddlyburge <ceddlyburge@gmail.com>
2021-01-28 13:15:07 +0000

    Remove calculator from test

--- tests/test_construction_margin_calculator_isolate_branches.py
+++ tests/test_construction_margin_calculator_isolate_branches.py
@@ -1,71 +1,6 @@
 from datetime import datetime
-from typing import List
-from dataclasses import dataclass
-
-@dataclass
-# This class contains a fictional list of rows in a cash flow calculation 
-# for a wind farm, only the properties used in the calculation below are 
-# present, but in reality there would be many more
-class CashFlowStep:
-    start_of_step: datetime
-    special_capital_costs: float
-    development_cost_if_owning: float
-    development_cost: float
-    turbine_cost_including_margin: float
-    balance_of_plant_costs_at_financial_close: float
-    construction_profit: float
-    balance_of_plant_cost_including_margin: float
-    # and lots of other properties
-
-# This class calculates a subset of properties on CashFlowSteps. In reality
-# there would be multiple similar classes, all setting properties on CashFlowStep
-class ConstructionMarginCashFlowCostCalculator:
-    def __init__(
-            self, 
-            balance_of_plant_costs_at_financial_close: float, 
-            development_cost: float, 
-            turbine_costs: float, 
-            special_capital_costs: float, 
-            date_of_financial_close: datetime, 
-            in_selling_mode: bool, 
-            epc_margin: float, 
-            inflation_calculator
-        ):
-        self.balance_of_plant_costs_at_financial_close = balance_of_plant_costs_at_financial_close
-        self.development_cost = development_cost
-        self.turbine_costs = turbine_costs
-        self.special_capital_costs = special_capital_costs
-        self.date_of_financial_close = date_of_financial_close
-        self.in_selling_mode = in_selling_mode
-        self.epc_margin = epc_margin
-        self.inflation_calculator = inflation_calculator
-
-    def calculate_step(self, step: CashFlowStep, fraction_of_spend: float):
-        inflation = self.inflation_calculator.inflation_to(step.start_of_step)
-
-        if step.start_of_step == self.date_of_financial_close:
-            step.special_capital_costs = self.special_capital_costs
-
-            if self.in_selling_mode == False:
-                step.development_cost_if_owning = self.development_cost
-
-            step.development_cost = self.development_cost
-
-        step.turbine_cost_including_margin = \
-            self.turbine_costs * inflation * fraction_of_spend
-        
-        step.balance_of_plant_cost_including_margin = \
-            self.balance_of_plant_costs_at_financial_close * inflation * fraction_of_spend
-
-        if self.in_selling_mode:
-            step.construction_profit = \
-                -1 * \
-                (step.turbine_cost_including_margin + step.balance_of_plant_cost_including_margin) * \
-                self.epc_margin
-            
-            step.turbine_cost_including_margin *= (1 + self.epc_margin)
-            
-            step.balance_of_plant_cost_including_margin *= (1 + self.epc_margin)
+from cash_flow_calculator.cash_flow_step import CashFlowStep
+from cash_flow_calculator.construction_margin_calculator_mockable_abstraction import ConstructionMarginCalculatorMockableAbstraction
 
 any_double = 5.55555
 
@@ -87,7 +22,7 @@ def test_calculates_turbine_cost_including_margin_correctly():
     date_of_financial_close = datetime(2020, 1, 1)
     not_date_of_financial_close = datetime(2020, 1, 2)
 
-    sut = ConstructionMarginCashFlowCostCalculator(
+    sut = ConstructionMarginCalculatorMockableAbstraction(
         balance_of_plant_costs_at_financial_close, 
         any_double, 
         turbine_costs, 
