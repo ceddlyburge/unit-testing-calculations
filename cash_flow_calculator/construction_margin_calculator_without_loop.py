db2e0fca82de837d9b42720ca77289ee582605d2
ceddlyburge <ceddlyburge@gmail.com>
2021-01-28 13:03:59 +0000

    Split calculator without loop from test and rename

---
+++ cash_flow_calculator/construction_margin_calculator_without_loop.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+from typing import List
+from dataclasses import dataclass
+from cash_flow_calculator.cash_flow_step import CashFlowStep
+
+# This class calculates a subset of properties on CashFlowSteps. In reality
+# there would be multiple similar classes, all setting properties on CashFlowStep
+class ConstructionMarginCalculatorWithoutLoop:
+    def __init__(
+            self, 
+            balance_of_plant_costs_at_financial_close: float, 
+            development_cost: float, 
+            turbine_costs: float, 
+            special_capital_costs: float, 
+            date_of_financial_close: datetime, 
+            in_selling_mode: bool, 
+            epc_margin: float, 
+            inflation_rate: float, 
+            inflation_mode: int
+        ):
+        self.balance_of_plant_costs_at_financial_close = balance_of_plant_costs_at_financial_close
+        self.development_cost = development_cost
+        self.turbine_costs = turbine_costs
+        self.special_capital_costs = special_capital_costs
+        self.date_of_financial_close = date_of_financial_close
+        self.in_selling_mode = in_selling_mode
+        self.epc_margin = epc_margin
+        self.inflation_rate = inflation_rate
+        self.inflation_mode = inflation_mode
+
+    def calculate_step(self, step: CashFlowStep, fraction_of_spend: float):
+        inflation = self.calculate_inflation(step.start_of_step)
+
+        if step.start_of_step == self.date_of_financial_close:
+            step.special_capital_costs = self.special_capital_costs
+
+            if self.in_selling_mode == False:
+                step.development_cost_if_owning = self.development_cost
+
+            step.development_cost = self.development_cost
+
+        step.turbine_cost_including_margin = \
+            self.turbine_costs * inflation * fraction_of_spend
+        
+        step.balance_of_plant_cost_including_margin = \
+            self.balance_of_plant_costs_at_financial_close * inflation * fraction_of_spend
+
+        if self.in_selling_mode:
+            step.construction_profit = \
+                -1 * \
+                (step.turbine_cost_including_margin + step.balance_of_plant_cost_including_margin) * \
+                self.epc_margin
+            
+            step.turbine_cost_including_margin *= (1 + self.epc_margin)
+            
+            step.balance_of_plant_cost_including_margin *= (1 + self.epc_margin)
+
+    def calculate_inflation(self, start_of_step: datetime) -> float:
+        # return 1 for the sake of the example, but in reality the calculation would need
+        # self.date_of_financial_close, self.inflation_rate and self.inflation_mode
+        return 1
+
+
+# This class is responsible for calculating a list of CashFlowStep, now that this 
+# responsibilty has been extracted from ConstructionMarginCashFlowCostCalculator
+class CashFlowStepsCalculator:
+    def __init__(self, steps: List[CashFlowStep]):
+        self.steps = steps
+
+    # calculator is something that has a calculate_step function here, we could add a 
+    # base class and type it if we wished to.
+    def calculate_step(self, calculator, fraction_of_spend: float):
+        for step in self.steps:
+            calculator.calculate_step(step, fraction_of_spend)
+
--- tests/test_construction_margin_calculator_remove_loop.py
+++ tests/test_construction_margin_calculator_remove_loop.py
@@ -1,90 +1,10 @@
 from datetime import datetime
 from typing import List
 from dataclasses import dataclass
-
-@dataclass
-# This class contains a fictional list of rows in a cash flow calculation 
-# for a wind farm, only the properties used in the calculation below are 
-# present, but in reality there would be many more
-class CashFlowStep:
-    start_of_step: datetime
-    special_capital_costs: float
-    development_cost_if_owning: float
-    development_cost: float
-    turbine_cost_including_margin: float
-    balance_of_plant_costs_at_financial_close: float
-    construction_profit: float
-    balance_of_plant_cost_including_margin: float
-    # and lots of other properties
-
-# This class calculates a subset of properties on CashFlowSteps. In reality
-# there would be multiple similar classes, all setting properties on CashFlowStep
-class ConstructionMarginCashFlowCostCalculator:
-    def __init__(
-            self, 
-            balance_of_plant_costs_at_financial_close: float, 
-            development_cost: float, 
-            turbine_costs: float, 
-            special_capital_costs: float, 
-            date_of_financial_close: datetime, 
-            in_selling_mode: bool, 
-            epc_margin: float, 
-            inflation_rate: float, 
-            inflation_mode: int
-        ):
-        self.balance_of_plant_costs_at_financial_close = balance_of_plant_costs_at_financial_close
-        self.development_cost = development_cost
-        self.turbine_costs = turbine_costs
-        self.special_capital_costs = special_capital_costs
-        self.date_of_financial_close = date_of_financial_close
-        self.in_selling_mode = in_selling_mode
-        self.epc_margin = epc_margin
-        self.inflation_rate = inflation_rate
-        self.inflation_mode = inflation_mode
-
-    def calculate_step(self, step: CashFlowStep, fraction_of_spend: float):
-        inflation = self.calculate_inflation(step.start_of_step)
-
-        if step.start_of_step == self.date_of_financial_close:
-            step.special_capital_costs = self.special_capital_costs
-
-            if self.in_selling_mode == False:
-                step.development_cost_if_owning = self.development_cost
-
-            step.development_cost = self.development_cost
-
-        step.turbine_cost_including_margin = \
-            self.turbine_costs * inflation * fraction_of_spend
-        
-        step.balance_of_plant_cost_including_margin = \
-            self.balance_of_plant_costs_at_financial_close * inflation * fraction_of_spend
-
-        if self.in_selling_mode:
-            step.construction_profit = \
-                -1 * \
-                (step.turbine_cost_including_margin + step.balance_of_plant_cost_including_margin) * \
-                self.epc_margin
-            
-            step.turbine_cost_including_margin *= (1 + self.epc_margin)
-            
-            step.balance_of_plant_cost_including_margin *= (1 + self.epc_margin)
-
-    def calculate_inflation(self, start_of_step: datetime) -> float:
-        # return 1 for the sake of the example, but in reality the calculation would need
-        # self.date_of_financial_close, self.inflation_rate and self.inflation_mode
-        return 1
-
-# This class calculates a subset of properties on a CashFlowStep. In reality
-# there would be multiple similar classes, all setting properties on CashFlowStep
-class CashFlowStepsCalculator:
-    def __init__(self, steps: List[CashFlowStep]):
-        self.steps = steps
-
-    # calculator is something that has a calculate_step function here, we could add a 
-    # base class and type it if we wished to.
-    def calculate_step(self, calculator, fraction_of_spend: float):
-        for step in self.steps:
-            calculator.calculate_step(step, fraction_of_spend)
+from cash_flow_calculator.cash_flow_step import CashFlowStep
+from cash_flow_calculator.construction_margin_calculator_without_loop import (
+    ConstructionMarginCalculatorWithoutLoop, 
+    CashFlowStepsCalculator)
 
 
 # The function now has 2^7 (128) paths in total, much less that the initial 2^9 (512) paths, 
@@ -102,7 +22,7 @@ def test_calculate_step_sets_correct_values():
     inflation_mode = 2
     fraction_of_spend = 0.3
 
-    sut = ConstructionMarginCashFlowCostCalculator(
+    sut = ConstructionMarginCalculatorWithoutLoop(
         balance_of_plant_costs_at_financial_close, 
         development_cost, 
         turbine_costs, 
