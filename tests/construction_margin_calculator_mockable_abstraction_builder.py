7a519a2d63f4c15a40a2dacf65095f2224fa6be6
ceddlyburge <ceddlyburge@gmail.com>
2021-02-01 17:37:48 +0000

    Split ConstructionMarginCalculatorMockableAbstractionBuilder to its own file

---
+++ tests/construction_margin_calculator_mockable_abstraction_builder.py
@@ -0,0 +1,37 @@
+from cash_flow_calculator.construction_margin_calculator_mockable_abstraction import ConstructionMarginCalculatorMockableAbstraction
+from tests.mock_inflation import MockInflation
+
+any_double = 5.55555
+
+class ConstructionMarginCalculatorMockableAbstractionBuilder:
+
+    def __init__(self): 
+        self._sut = ConstructionMarginCalculatorMockableAbstraction(
+        balance_of_plant_costs_at_financial_close=any_double, 
+        development_cost=any_double, 
+        turbine_costs=any_double, 
+        special_capital_costs=any_double, 
+        date_of_financial_close=None, 
+        in_selling_mode=None, 
+        epc_margin=any_double, 
+        inflation_calculator=MockInflation(any_double)
+    )
+
+    def with_balance_of_plant_costs_at_financial_close(self, balance_of_plant_costs_at_financial_close: float): 
+        self._sut.balance_of_plant_costs_at_financial_close = balance_of_plant_costs_at_financial_close
+        return self
+
+    def with_inflation(self, inflation: float): 
+        self._sut.inflation_calculator = MockInflation(inflation)
+        return self
+
+    def with_epc_margin(self, epc_margin: float): 
+        self._sut.epc_margin = epc_margin
+        return self
+
+    def in_selling_mode(self): 
+        self._sut.in_selling_mode = True
+        return self
+
+    def build(self):
+        return self._sut
--- tests/test_construction_margin_calculator_isolate_values.py
+++ tests/test_construction_margin_calculator_isolate_values.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from cash_flow_calculator.cash_flow_step import CashFlowStep
-from cash_flow_calculator.construction_margin_calculator_mockable_abstraction import ConstructionMarginCalculatorMockableAbstraction
-from tests.mock_inflation import MockInflation
+from tests.construction_margin_calculator_mockable_abstraction_builder import ConstructionMarginCalculatorMockableAbstractionBuilder
 from tests.cash_flow_step_builder import CashFlowStepBuilder
 
 # Isolating the balance_of_plant_cost_including_margin value allows the test to 
@@ -23,7 +22,7 @@ def test_calculates_balance_of_plant_cost_including_margin_correctly_when_in_sel
     epc_margin = 0.1
     inflation = 1.2
 
-    sut = ConstructionMarginCashFlowCostCalculatorBuilder() \
+    sut = ConstructionMarginCalculatorMockableAbstractionBuilder() \
         .with_balance_of_plant_costs_at_financial_close(balance_of_plant_costs_at_financial_close) \
         .with_inflation(inflation) \
         .with_epc_margin(epc_margin) \
@@ -35,43 +34,6 @@ def test_calculates_balance_of_plant_cost_including_margin_correctly_when_in_sel
     sut.calculate_step(cash_flow_step, fraction_of_spend)
 
     assert cash_flow_step.balance_of_plant_cost_including_margin == (balance_of_plant_costs_at_financial_close * inflation * fraction_of_spend) * (1 + epc_margin)
-
-
-any_double = 5.55555
-
-
-class ConstructionMarginCashFlowCostCalculatorBuilder:
-
-    def __init__(self): 
-        self._sut = ConstructionMarginCalculatorMockableAbstraction(
-        balance_of_plant_costs_at_financial_close=any_double, 
-        development_cost=any_double, 
-        turbine_costs=any_double, 
-        special_capital_costs=any_double, 
-        date_of_financial_close=None, 
-        in_selling_mode=None, 
-        epc_margin=any_double, 
-        inflation_calculator=MockInflation(any_double)
-    )
-
-    def with_balance_of_plant_costs_at_financial_close(self, balance_of_plant_costs_at_financial_close: float): 
-        self._sut.balance_of_plant_costs_at_financial_close = balance_of_plant_costs_at_financial_close
-        return self
-
-    def with_inflation(self, inflation: float): 
-        self._sut.inflation_calculator = MockInflation(inflation)
-        return self
-
-    def with_epc_margin(self, epc_margin: float): 
-        self._sut.epc_margin = epc_margin
-        return self
-
-    def in_selling_mode(self): 
-        self._sut.in_selling_mode = True
-        return self
-
-    def build(self):
-        return self._sut
 
 
 # The code and test for the CashFlowStepsCalculator is no longer shown
